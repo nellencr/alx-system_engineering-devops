4fd1d6f830c9ff8ae5ae90f67428fce0a3cdac95
NELA <nellencr@gmail.com>
2023-03-24 20:12:42 +0200

    Update 1-install_a_package.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,5 @@
-# Install the puppet-lint package
+# Installing a package with a particular version
 package { 'puppet-lint':
   ensure   => '2.5.0',
-  provider => 'gem',
-}
+  provider => gem;
+  }
